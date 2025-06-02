5e5beaf7f602aa2dd33edbc14218ec8dd4675763
mythsoul <mythsoul2023@gmail.com>
2025-06-02 09:40:00 +0000

    fixed all the issues

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -5,4 +5,26 @@ const socket = io(String(import.meta.env.VITE_SOCKET_SERVER), {
   reconnectionAttempts: 5
 });
 
+// Debug socket connection
+socket.on('connect', () => {
+  console.log('🔌 Socket connected:', socket.id);
+});
+
+socket.on('disconnect', () => {
+  console.log('🔌 Socket disconnected');
+});
+
+socket.on('connect_error', (error) => {
+  console.error('🔌 Socket connection error:', error);
+});
+
+socket.on('reconnect', () => {
+  console.log('🔌 Socket reconnected:', socket.id);
+});
+
+// Debug all events
+socket.onAny((event, ...args) => {
+  console.log('📞 Received socket event:', event, args);
+});
+
 export default socket;
