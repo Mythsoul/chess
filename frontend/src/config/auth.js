d539ff0cacf8c2587799c6f11a24b5a28a4a648f
Mythsoul <mythsoul2023@gmail.com>
2025-06-15 14:30:00 +0000

    fixed auth server issue

--- frontend/src/config/auth.js
+++ frontend/src/config/auth.js
@@ -2,8 +2,7 @@ import { configure } from 'easy.auth98'
 
 // Configure EasyAuth SDK
 configure({
-  // Using the default hosted EasyAuth service
-  baseURL: import.meta.env.VITE_EASY_AUTH_BASE_URL || 'https://auth.easy.com',
+  
   timeout: 15000,
   tokenCookies: {
     access: 'chess_access_token',
